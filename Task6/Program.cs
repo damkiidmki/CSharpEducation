6bb685b65ceafd1d7dfbd3cf254a0a817435ea86
Dima <damkiidmki@yandex.ru>
2024-07-26 14:22:42 +0330

    Задание 5. Программа, которая вычисляет формулу площади круга и выводит результат на экран консоли с понятным сообщением

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿const double Pi = Math.PI; 
+
+var radius = 2d;
+
+Console.WriteLine($"Площадь круга с радиусом ({radius} см) равна {AreaOfCircle()} см^2");
+
+double AreaOfCircle()
+{
+    return Pi * Math.Pow(radius, 2);
+}
