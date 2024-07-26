1e34fa6212cd9469d493b720784489d8e1af465d
Dima <damkiidmki@yandex.ru>
2024-07-26 14:23:17 +0330

    Задание 6. Программа, которая вычисляет длину катетов и гипотенузы

---
+++ Task7/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var legOne = 3d;
+        var legTwo = 4d;
+        var hypotenuse = 5d;
+        Console.WriteLine(GetHypotenuse(legTwo, legOne));
+        Console.WriteLine(GetLeg(hypotenuse, legOne));
+    }
+
+    private static double GetHypotenuse(double legOne, double legTwo)
+    {
+        return Math.Sqrt(Math.Pow(legOne, 2) + Math.Pow(legTwo, 2));
+    }
+
+    private static double GetLeg(double hypotenuse, double leg)
+    {
+        return Math.Sqrt(Math.Pow(hypotenuse, 2) - Math.Pow(leg, 2));
+    }
+}
+
